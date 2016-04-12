e79d0713e1fd6371ea64f3a2e76a1eff4f81a536
Syoyo Fujita <syoyo@lighttransport.com>
2016-04-13 00:47:55 +0900

    Remove ill-defined syntax in C++-03

--- nanort.h
+++ nanort.h
@@ -293,58 +293,62 @@ class StackVector
 struct float3 {
   float3() {}
   float3(float xx, float yy, float zz) {
-    x = xx;
-    y = yy;
-    z = zz;
+    v[0] = xx;
+    v[1] = yy;
+    v[2] = zz;
   }
   explicit float3(const float *p) {
-    x = p[0];
-    y = p[1];
-    z = p[2];
+    v[0] = p[0];
+    v[1] = p[1];
+    v[2] = p[2];
   }
 
-  float3 operator*(float f) const { return float3(x * f, y * f, z * f); }
+  inline float x() const { return v[0]; }
+  inline float y() const { return v[1]; }
+  inline float z() const { return v[2]; }
+
+  float3 operator*(float f) const { return float3(x() * f, y() * f, z() * f); }
   float3 operator-(const float3 &f2) const {
-    return float3(x - f2.x, y - f2.y, z - f2.z);
+    return float3(x() - f2.x(), y() - f2.y(), z() - f2.z());
   }
   float3 operator*(const float3 &f2) const {
-    return float3(x * f2.x, y * f2.y, z * f2.z);
+    return float3(x() * f2.x(), y() * f2.y(), z() * f2.z());
   }
   float3 operator+(const float3 &f2) const {
-    return float3(x + f2.x, y + f2.y, z + f2.z);
+    return float3(x() + f2.x(), y() + f2.y(), z() + f2.z());
   }
   float3 &operator+=(const float3 &f2) {
-    x += f2.x;
-    y += f2.y;
-    z += f2.z;
+    v[0] += f2.x();
+    v[1] += f2.y();
+    v[2] += f2.z();
     return (*this);
   }
   float3 operator/(const float3 &f2) const {
-    return float3(x / f2.x, y / f2.y, z / f2.z);
+    return float3(x() / f2.x(), y() / f2.y(), z() / f2.z());
   }
-  float operator[](int i) const { return (&x)[i]; }
-  float &operator[](int i) { return (&x)[i]; }
+  float operator[](int i) const { return v[i]; }
+  float &operator[](int i) { return v[i]; }
 
-  float3 neg() { return float3(-x, -y, -z); }
+  float3 neg() { return float3(-x(), -y(), -z()); }
 
-  float length() { return sqrtf(x * x + y * y + z * z); }
+  float length() { return sqrtf(x() * x() + y() * y() + z() * z()); }
 
   void normalize() {
     float len = length();
     if (fabsf(len) > 1.0e-6f) {
       float inv_len = 1.0f / len;
-      x *= inv_len;
-      y *= inv_len;
-      z *= inv_len;
+      v[0] *= inv_len;
+      v[1] *= inv_len;
+      v[2] *= inv_len;
     }
   }
 
-  float x, y, z;
+  float v[3];
   // float pad;  // for alignment
 };
 
 inline float3 operator*(float f, const float3 &v) {
-  return float3(v.x * f, v.y * f, v.z * f);
+  return float3(v.x() * f, v.y() * f, v.z() * f);
 }
 
 inline float3 vcross(float3 a, float3 b) {
