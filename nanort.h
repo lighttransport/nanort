f70fe7f466928b66e5580b9b3b738987463c1ab0
Syoyo Fujita <syoyo@lighttransport.com>
2022-11-17 03:56:56 +0900

    Add TheadPool code.

--- nanort.h
+++ nanort.h
@@ -51,6 +51,7 @@ THE SOFTWARE.
 // compiler macros
 //
 // NANORT_USE_CPP11_FEATURE : Enable C++11 feature
+// NANORT_USE_CPP11_THREADPOOL: Enable C++11 threadpool feature
 // NANORT_ENABLE_PARALLEL_BUILD : Enable parallel BVH build.
 // NANORT_ENABLE_SERIALIZATION : Enable serialization feature for built BVH.
 //
@@ -64,13 +65,19 @@ THE SOFTWARE.
 #define kNANORT_MIN_PRIMITIVES_FOR_PARALLEL_BUILD (1024 * 8)
 #define kNANORT_SHALLOW_DEPTH (4)  // will create 2**N subtrees
 
-#ifdef NANORT_USE_CPP11_FEATURE
+#if defined(NANORT_USE_CPP11_FEATURE) || defined(NANORT_USE_CPP11_THREADPOOL)
 // Assume C++11 compiler has thread support.
 // In some situation (e.g. embedded system, JIT compilation), thread feature
 // may not be available though...
 #include <atomic>
 #include <mutex>
 #include <thread>
+
+#ifdef NANORT_USE_CPP11_THREADPOOL
+#include <condition_variable>
+#include <future>
+#include <functional>
+#endif
 
 #define kNANORT_MAX_THREADS (256)
 
@@ -82,6 +89,128 @@ THE SOFTWARE.
 #endif
 
 namespace nanort {
+
+#if defined(NANORT_USE_CPP11_THREADPOOL)
+
+// A simple C++11 Thread Pool implementation. ----------------------------------
+// https://github.com/progschj/ThreadPool
+
+/*
+Copyright (c) 2012 Jakob Progsch, Václav Zeman
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+   3. This notice may not be removed or altered from any source
+   distribution.
+*/
+
+class ThreadPool {
+public:
+    ThreadPool(size_t);
+    template<class F, class... Args>
+    auto enqueue(F&& f, Args&&... args) 
+        -> std::future<typename std::result_of<F(Args...)>::type>;
+    ~ThreadPool();
+private:
+    // need to keep track of threads so we can join them
+    std::vector< std::thread > workers;
+    // the task queue
+    std::queue< std::function<void()> > tasks;
+    
+    // synchronization
+    std::mutex queue_mutex;
+    std::condition_variable condition;
+    bool stop;
+};
+ 
+// the constructor just launches some amount of workers
+inline ThreadPool::ThreadPool(size_t threads)
+    :   stop(false)
+{
+    for(size_t i = 0;i<threads;++i)
+        workers.emplace_back(
+            [this]
+            {
+                for(;;)
+                {
+                    std::function<void()> task;
+
+                    {
+                        std::unique_lock<std::mutex> lock(this->queue_mutex);
+                        this->condition.wait(lock,
+                            [this]{ return this->stop || !this->tasks.empty(); });
+                        if(this->stop && this->tasks.empty())
+                            return;
+                        task = std::move(this->tasks.front());
+                        this->tasks.pop();
+                    }
+
+                    task();
+                }
+            }
+        );
+}
+
+// add new work item to the pool
+template<class F, class... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args) 
+    -> std::future<typename std::result_of<F(Args...)>::type>
+{
+    using return_type = typename std::result_of<F(Args...)>::type;
+
+    auto task = std::make_shared< std::packaged_task<return_type()> >(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+        );
+        
+    std::future<return_type> res = task->get_future();
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+
+#if 0
+        // don't allow enqueueing after stopping the pool
+        if(stop)
+            throw std::runtime_error("enqueue on stopped ThreadPool");
+
+        tasks.emplace([task](){ (*task)(); });
+#else
+        if (!stop) {
+          tasks.emplace([task](){ (*task)(); });
+        }
+#endif
+    }
+    condition.notify_one();
+    return res;
+}
+
+// the destructor joins all threads
+inline ThreadPool::~ThreadPool()
+{
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        stop = true;
+    }
+    condition.notify_all();
+    for(std::thread &worker: workers)
+        worker.join();
+}
+
+// end ThreadPool.h ---------------------------------------------------
+
+#endif
 
 // RayType
 typedef enum {
